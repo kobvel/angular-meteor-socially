f2e89ce9feec3d6fab88473557852635268e9cc5
Mykhailo <kobvel@gmail.com>
2015-10-31 14:16:11 +0200

    pagination added

--- socially/client/parties/controllers/partiesList.ng.js
+++ socially/client/parties/controllers/partiesList.ng.js
@@ -1,7 +1,31 @@
 angular
     .module("socially")
     .controller("PartiesListCtrl", function ($scope, $meteor) {
-        $scope.parties = $meteor.collection(Parties).subscribe('parties');
+        $scope.parties = $meteor.collection(function () {
+            return Parties.find({}, {
+                sort: $scope.getReactively('sort')
+            });
+        });
+        $scope.orderProperty = '1';
+        $scope.page = 1;
+        $scope.perPage = 3;
+        $scope.sort = {name: 1};
+
+        $meteor.autorun($scope, function () {
+            $meteor.subscribe('parties', {
+                limit: parseInt($scope.getReactively('perPage')),
+                skip: (parseInt($scope.getReactively('page')) - 1) * parseInt($scope.getReactively('perPage')),
+                sort: $scope.getReactively('sort')
+            }, $scope.getReactively('search')).then(function () {
+                $scope.partiesCount = $meteor.object(Counts, 'numberOfParties', false);
+            });
+        });
+
+        $scope.$watch('orderProperty', function () {
+            if ($scope.orderProperty)
+                $scope.sort = {name: parseInt($scope.orderProperty)};
+        });
+
 
         $scope.remove = function (party) {
             $scope.parties.splice($scope.parties.indexOf(party), 1);
@@ -9,5 +33,9 @@ angular
 
         $scope.removeAll = function () {
             $scope.parties.remove();
+        };
+
+        $scope.pageChanged = function (newPage) {
+            $scope.page = newPage;
         };
     });
--- socially/client/parties/controllers/partyDetails.ng.js
+++ socially/client/parties/controllers/partyDetails.ng.js
@@ -1,6 +1,7 @@
 angular
     .module("socially")
     .controller("PartyDetailsCtrl", function ($scope, $stateParams, $meteor) {
-        $scope.party = $meteor.object(Parties, $stateParams.partyId).subscribe('parties');
+        $scope.party = $meteor.object(Parties, $stateParams.partyId);
         $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
+        $scope.$meteorSubscribe('parties');
     });
