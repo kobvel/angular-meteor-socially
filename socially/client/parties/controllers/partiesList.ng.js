3cf86ca14bc9b4e63e9ee83948f9f223c8f8e5fb
Mykhailo <kobvel@gmail.com>
2015-10-31 20:42:08 +0200

    Google maps added

--- socially/client/parties/controllers/partiesList.ng.js
+++ socially/client/parties/controllers/partiesList.ng.js
@@ -1,6 +1,6 @@
 angular
     .module("socially")
-    .controller("PartiesListCtrl", function ($scope, $meteor, $rootScope) {
+    .controller("PartiesListCtrl", function ($scope, $meteor, $rootScope, $state) {
         $scope.parties = $meteor.collection(function () {
             return Parties.find({}, {
                 sort: $scope.getReactively('sort')
@@ -22,6 +22,21 @@ angular
                 $scope.partiesCount = $meteor.object(Counts, 'numberOfParties', false);
             });
         });
+
+        $scope.parties.forEach(function (party) {
+            party.onClicked = function () {
+                $state.go('partyDetails', {partyId: party._id});
+            };
+        });
+
+        $scope.map = {
+            center: {
+                latitude: 45,
+                longitude: -73
+            },
+            zoom: 8
+        };
+
 
         $scope.$watch('orderProperty', function () {
             if ($scope.orderProperty)
