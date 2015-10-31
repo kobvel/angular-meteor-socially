339f7a51083594f25987ab5913ab844e6c80a7c7
Mykhailo <kobvel@gmail.com>
2015-10-31 14:29:25 +0200

    regExp find via MongoDB from client side

--- socially/client/parties/controllers/partiesList.ng.js
+++ socially/client/parties/controllers/partiesList.ng.js
@@ -1,6 +1,6 @@
 angular
     .module("socially")
-    .controller("PartiesListCtrl", function ($scope, $meteor) {
+    .controller("PartiesListCtrl", function ($scope, $meteor, $rootScope) {
         $scope.parties = $meteor.collection(function () {
             return Parties.find({}, {
                 sort: $scope.getReactively('sort')
@@ -10,6 +10,8 @@ angular
         $scope.page = 1;
         $scope.perPage = 3;
         $scope.sort = {name: 1};
+        
+        $scope.$meteorSubscribe('users');
 
         $meteor.autorun($scope, function () {
             $meteor.subscribe('parties', {
@@ -26,6 +28,24 @@ angular
                 $scope.sort = {name: parseInt($scope.orderProperty)};
         });
 
+        $scope.getUserById = function (userId) {
+            return Meteor.users.findOne(userId);
+        };
+
+        $scope.creator = function (party) {
+            if (!party)
+                return;
+            var owner = $scope.getUserById(party.owner);
+            if (!owner)
+                return 'nobody';
+
+            if ($rootScope.currentUser)
+                if ($rootScope.currentUser._id)
+                    if (owner._id === $rootScope.currentUser._id)
+                        return 'me';
+
+            return owner;
+        };
 
         $scope.remove = function (party) {
             $scope.parties.splice($scope.parties.indexOf(party), 1);
