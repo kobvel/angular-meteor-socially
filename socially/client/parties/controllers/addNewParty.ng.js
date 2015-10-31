f06c37170b7721a51804b00191ce116f514a7fca
Mykhailo <kobvel@gmail.com>
2015-10-31 21:16:43 +0200

    restyling using LESS

---
+++ socially/client/parties/controllers/addNewParty.ng.js
@@ -0,0 +1,18 @@
+angular
+    .module("socially")
+    .controller("AddNewPartyCtrl", [
+        '$scope',
+        '$meteor',
+        '$rootScope',
+        '$state',
+        '$modalInstance',
+        'parties',
+        function ($scope, $meteor, $rootScope, $state, $modalInstance, parties) {
+
+            $scope.addNewParty = function () {
+                $scope.newParty.owner = $rootScope.currentUser._id;
+                parties.push($scope.newParty);
+                $scope.newParty = '';
+                $modalInstance.close();
+            }
+        }]);
