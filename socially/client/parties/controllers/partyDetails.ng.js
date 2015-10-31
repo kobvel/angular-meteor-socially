81737283427549f1be03ddb898ce784f47649722
Mykhailo <kobvel@gmail.com>
2015-10-31 13:17:54 +0200

    additional functionality

---
+++ socially/client/parties/controllers/partyDetails.ng.js
@@ -0,0 +1,6 @@
+angular
+    .module("socially")
+    .controller("PartyDetailsCtrl", function ($scope, $stateParams, $meteor) {
+        $scope.party = $meteor.object(Parties, $stateParams.partyId).subscribe('parties');
+        $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
+    });
